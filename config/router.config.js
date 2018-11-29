a3204351a7808dc04f84f01197dd805db6cec9cb
Jack Xie <jxie@riotgames.com>
2018-11-28 18:43:24 -0800

    change /user', redirect: '/user/guest

--- config/router.config.js
+++ config/router.config.js
@@ -4,7 +4,7 @@ export default [
     path: '/user',
     component: '../layouts/UserLayout',
     routes: [
-      { path: '/user', redirect: '/user/login' },
+      { path: '/user', redirect: '/user/guest' },
       { path: '/user/login', component: './User/Login' },
       { path: '/user/register', component: './User/Register' },
       { path: '/user/register-result', component: './User/RegisterResult' },
